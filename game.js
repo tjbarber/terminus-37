ebbec20274223b133be9644d2674fac361cb6226
T.J. Barber <me@tjbarber.org>
2013-11-11 18:48:19 -0600

    added color and cleaned up the code

--- game.js
+++ game.js
@@ -1,29 +1,13 @@
 var ncurses = require("ncurses"),
 	spaceCraft = require("./lib/spacecraft"),
-	win = new ncurses.Window();
+	mainWin = new ncurses.Window(),
+	craftWin = new ncurses.Window(ncurses.lines, ncurses.cols);
 
 ncurses.raw = true;
 ncurses.showCursor = false;
 
-var attrs = win.attrs,
-	colors = win.colors,
-	gameOn = false,
-	lines = ncurses.lines,
-	craft = spaceCraft.init(lines);
+var attrs = ncurses.attrs,
+	colors = ncurses.colors,
+	lines = ncurses.lines;
 
-win.on("inputChar", function(character, inputChar, isKey) {
-	if (inputChar === 27) {
-		win.close();
-	} else if (gameOn === true && inputChar === 261 && craftX < ncurses.cols - 6) {
-		/* pressing right */
-		spaceCraft.moveRight(win, ncurses);
-	} else if (gameOn === true && inputChar === 260 && craftX > 1) {
-		/* pressing left */
-		spaceCraft.moveLeft(win, ncurses);
-	}
-});
-
-win.print(position, craftX, craft);
-gameOn = true;
-win.refresh();
-
+spaceCraft.init(mainWin, craftWin, ncurses);
